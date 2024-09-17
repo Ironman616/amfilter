fb8efaeef26d48cff0f2794d8b753685a9cb36b8
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2024-09-17 12:27:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,7 +57,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://lokimava1122:8dl8cYMSeoWuwvKN@lokimava.os0mf.mongodb.net/?retryWrites=true&w=majority&appName=lokimava")
 DATABASE_NAME = environ.get('DATABASE_NAME', "lokimava1122")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files)
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Premium And Referal Settings
 PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', True)) # Set Ture Or False
